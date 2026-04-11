c7e68f87cc8078af33a159ad2d4b8277b91a097d
lucasneitor <juanitoalcachofashd@gmail.com>
2026-04-10 22:19:39 -0300

    Municion para movimiento

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -7,6 +7,10 @@ public class PlayerMovement : MonoBehaviour
     public float fuerzaRetroceso = 15f;
     public float velocidadMaximaX = 8f;
 
+    [Header("Munición")]
+    public int balasIzquierda = 10;
+    public int balasDerecha = 10;
+
     private Rigidbody2D rb;
     private bool inputIzquierda;
     private bool inputDerecha;
@@ -20,40 +24,46 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        inputIzquierda = Input.GetKey(KeyCode.LeftArrow);
-        inputDerecha = Input.GetKey(KeyCode.RightArrow);
-        if (inputIzquierda && PuedeDisparar()) {
+        inputIzquierda = Input.GetKeyDown(KeyCode.LeftArrow);//aca cambie el input.getkey por el input.getkeydown porque al apretarse cualquier boton de movimiento... Lucas
+        inputDerecha = Input.GetKeyDown(KeyCode.RightArrow);//...se gastaba una bala por frame al mantener apretado y como corre a muchos FPS, se quedaba sin balas en un toque. Lucas
+
+        if (inputIzquierda && PuedeDispararIzquierda())
+        {
             Disparar(Vector2.left);
+            balasIzquierda--;
         }
-        else if (inputDerecha && PuedeDisparar()) {
+        // aca saque el else por una cuestion de que no me esta dejando moverme una vez que dispare hacie algun lado. Lucas
+        if (inputDerecha && PuedeDispararDerecha())
+        {
             Disparar(Vector2.right);
+            balasDerecha--;
         }
     }
 
     void FixedUpdate()
     {
-        
-        if (inputIzquierda && PuedeDisparar())
-        {
-            rb.AddForce(Vector2.right * fuerzaRetroceso, ForceMode2D.Force);
-        }
-        else if (inputDerecha && PuedeDisparar())
-        {
-            rb.AddForce(Vector2.left * fuerzaRetroceso, ForceMode2D.Force);
-        }
-        
-        if (Mathf.Abs(rb.linearVelocity.x) > velocidadMaximaX)
+        if (Mathf.Abs(rb.linearVelocity.x) > velocidadMaximaX)// aca tuve que simplificar la funcion porque estaba de mas el if inputizquierda / ...derecha. Lucas
         {
             rb.linearVelocity = new Vector2(Mathf.Sign(rb.linearVelocity.x) * velocidadMaximaX, rb.linearVelocity.y);
         }
     }
 
-    bool PuedeDisparar()
+    bool PuedeDispararIzquierda()
     {
-        return true; 
+        return balasIzquierda > 0;
+    }
+
+    bool PuedeDispararDerecha()
+    {
+        return balasDerecha > 0;
     }
 
     void Disparar(Vector2 direccion)
     {
+        // Aplica fuerza en sentido contrario
+        rb.AddForce(-direccion * fuerzaRetroceso, ForceMode2D.Impulse);
+
+        // Acá podés agregar efectos, sonido, etc.
+        Debug.Log("Disparo hacia: " + direccion);
     }
 }
